629a8f04316439b84b73f5391fdd441ac8d498f3
cale312 <zikisacalesogaxa@gmail.com>
2017-08-17 18:42:51 +0200

    to be continued

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use('/public', express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // change promise library
 mongoose.Promise = global.Promise;
@@ -13,7 +14,7 @@ mongoose.Promise = global.Promise;
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept"');
   next();
 })
 
